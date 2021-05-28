89c2e41b02036fbebcfbd880bc94edfd864369d8
ozichko <hsevket83@gmail.com>
2021-05-28 16:04:28 +0200

    first commit

---
+++ cardPlay/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace cardPlay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblscore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.flwPictures2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flwPictures = new System.Windows.Forms.FlowLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(211, 116);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(20, 24);
+            this.lblTime.TabIndex = 5;
+            this.lblTime.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "SURE :";
+            // 
+            // lblscore
+            // 
+            this.lblscore.AutoSize = true;
+            this.lblscore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblscore.Location = new System.Drawing.Point(211, 82);
+            this.lblscore.Name = "lblscore";
+            this.lblscore.Size = new System.Drawing.Size(20, 24);
+            this.lblscore.TabIndex = 9;
+            this.lblscore.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 24);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Skor : ";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(1022, 92);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(156, 57);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "BASLAT";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // flwPictures2
+            // 
+            this.flwPictures2.Location = new System.Drawing.Point(22, 418);
+            this.flwPictures2.Name = "flwPictures2";
+            this.flwPictures2.Size = new System.Drawing.Size(1266, 173);
+            this.flwPictures2.TabIndex = 3;
+            // 
+            // flwPictures
+            // 
+            this.flwPictures.Location = new System.Drawing.Point(22, 207);
+            this.flwPictures.Name = "flwPictures";
+            this.flwPictures.Size = new System.Drawing.Size(1266, 167);
+            this.flwPictures.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 2000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(578, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 24);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "LEVEL :";
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLevel.Location = new System.Drawing.Point(706, 36);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(20, 24);
+            this.lblLevel.TabIndex = 5;
+            this.lblLevel.Text = "0";
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 3000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1348, 771);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblscore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.flwPictures2);
+            this.Controls.Add(this.flwPictures);
+            this.Name = "Form1";
+            this.Text = "7";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblscore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.FlowLayoutPanel flwPictures2;
+        private System.Windows.Forms.FlowLayoutPanel flwPictures;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ cardPlay/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace cardPlay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region ConstantVariables
+        //in order to be able to catch clicked Pictureboxes and to compare them eachothers, these variables has been defined as global variables. 
+        PictureBox clicked;
+        PictureBox clicked2;
+        int clicked2Tag;
+        int clickedTag;
+        bool showImage1 = false;
+        bool showImage2 = false;
+        int time = 100;
+        int score = 0;
+        int level = 1;
+        
+        
+        
+
+
+        #endregion
+
+        #region OperationalMethods
+        // for getting dynamic directory path 
+        public string getDirectoryPath()
+        {
+            string fullCurrentDirectory = Directory.GetCurrentDirectory();
+            string trimmedDirectoryPath = fullCurrentDirectory.Replace(@"\", "/");
+            return trimmedDirectoryPath.Substring(0, fullCurrentDirectory.Length - 27);
+
+        }
+
+        /// <summary>
+        /// gamecard initializer  
+        /// </summary>
+        /// <param name="isTop">boolean parameter for defining flowlayout panel where the pictures will be placed </param>
+        /// <param name="numberOfCard">integer that define the card number (defined as default 10) </param>
+        public void generateCard(bool isTop, int numberOfCard = 10)
+        {
+
+            string currentDirectory = getDirectoryPath();
+            Random rdm = new Random();
+            //for preventing reiterated numbers, a list has been created.
+            List<int> list = new List<int>();
+            for (int i = 1; i <= numberOfCard; i++)
+            {
+                int rand = rdm.Next(1, numberOfCard + 1);
+
+                PictureBox pict = new PictureBox();
+                pict.Width = 110;
+                pict.Height = 150;
+                pict.BorderStyle = BorderStyle.Fixed3D;
+                while (list.Contains(rand))
+                    rand = rdm.Next(1, 11);
+                pict.BackgroundImage = Image.FromFile($"{currentDirectory}images/{rand}.jpg");
+                pict.BackgroundImageLayout = ImageLayout.Stretch;
+                pict.Image = Image.FromFile($"{currentDirectory}images/background.jpg");
+                pict.SizeMode = PictureBoxSizeMode.StretchImage;
+                pict.Tag = new { tag = rand };
+
+                //
+                if (isTop)
+                {
+                    flwPictures.Controls.Add(pict);
+                    list.Add(rand);
+                    pict.Click += Pict_Click;
+                }
+                else
+                {
+                    flwPictures2.Controls.Add(pict);
+                    list.Add(rand);
+                    pict.Click += Pict2_Click;
+                }
+            }
+            
+        }
+        /// <summary>
+        /// in case of game over, this dialog box asks the player reload the game
+        /// </summary>
+        /// <param name="message">the parameter that convey the message about game ending (time over, win or lose)</param>
+        public void gameOver(string message)
+        {
+            timer1.Stop();
+            flwPictures.Enabled = false;
+            flwPictures2.Enabled = false;
+            MessageBox.Show(message);
+            bool playAgain = GetMessageResult("Would you like to Play Again?", "Game");
+
+            if (playAgain)
+            {
+                startTheGame();
+            }
+        }
+        public void startTheGame()
+        {
+            flwPictures.Controls.Clear();
+            flwPictures2.Controls.Clear();
+            generateCard(true);
+            generateCard(false);
+            time = 100;
+            lblTime.Text = time.ToString();
+            score = 0;
+            lblscore.Text = score.ToString();
+            level = 1;
+            lblLevel.Text = level.ToString();
+            flwPictures.Enabled = true;
+            flwPictures2.Enabled = true;
+            clicked = null;
+            clicked2 = null;
+            showAllImages();
+            timer3.Start();
+            timer1.Start();
+
+        }
+
+        public void showAllImages()
+        {
+            foreach (PictureBox item in flwPictures.Controls)
+                item.Image = null;
+            foreach (PictureBox item2 in flwPictures2.Controls)
+                item2.Image = null;
+
+        }
+        public void hideAllImages()
+        {
+            string currentDirectory = getDirectoryPath();
+            foreach (PictureBox item in flwPictures.Controls)
+                item.Image = Image.FromFile($"{currentDirectory}images/background.jpg"); ;
+            foreach (PictureBox item2 in flwPictures2.Controls)
+                item2.Image = Image.FromFile($"{currentDirectory}images/background.jpg"); ;
+
+        }
+        public void levelUp()
+        {
+            MessageBox.Show($"{level + 1}. Level");
+            flwPictures.Controls.Clear();
+            flwPictures2.Controls.Clear();
+            generateCard(true);
+            generateCard(false);
+            level++;
+            lblLevel.Text = level.ToString();
+            time = 100 - (level * 5);
+            lblTime.Text = time.ToString();
+            flwPictures.Enabled = true;
+            flwPictures2.Enabled = true;
+            clicked = null;
+            clicked2 = null;
+            showAllImages();
+            timer3.Start();
+            timer1.Start();
+            if (level == 5)
+                gameOver("congratulation, we have winner!!! ");
+        }
+
+        /// <summary>
+        /// to compare the clicked pictures, to increase the score in case of matching pictures, to understand which flowlayout panel's picturebox clicked first  
+        /// </summary>
+        /// <param name="currentImage">home image for the click event that call this method  (exmp: click1, showImage1) </param>
+        /// <param name="otherImage">second image for the click event (example: click1, showImage2) </param>
+        public void compareImages(bool currentImage, bool otherImage)
+        {
+            if (otherImage)
+            {
+                if (clickedTag == clicked2Tag)
+                {
+                    clicked.Image = null;
+                    clicked2.Image = null;
+                    flwPictures.Enabled = true;
+                    flwPictures2.Enabled = true;
+                    score += 50;
+                    lblscore.Text = score.ToString();
+                    timer2.Stop();
+                    if (score != 0 && score % 500 == 0)
+                        levelUp();
+                }
+                else
+                    timer2.Start();
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message">the question</param>
+        /// <param name="caption"></param>
+        /// <returns></returns>
+        bool GetMessageResult(string message, string caption)
+        {
+
+            DialogResult dialogResult = MessageBox.Show(message, caption, MessageBoxButtons.YesNo);
+
+            return dialogResult == DialogResult.Yes ? true : false;
+        }
+        #endregion
+
+        #region Events
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            generateCard(true);
+            generateCard(false);
+            timer1.Stop();
+            
+        }
+
+        #region ClickEvents
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            startTheGame();
+        }
+
+        private void Pict2_Click(object sender, EventArgs e)
+        {
+            clicked2 = (PictureBox)sender;
+            clicked2.Image = null;
+            showImage2 = true;
+            flwPictures2.Enabled = false;
+            clicked2Tag = (int)clicked2.Tag.GetType().GetProperty("tag").GetValue(clicked2.Tag);
+            compareImages(showImage2, showImage1);
+            
+        }
+
+        private void Pict_Click(object sender, EventArgs e)
+        {
+            clicked = (PictureBox)sender;
+            clicked.Image = null;
+            showImage1 = true;
+            flwPictures.Enabled = false;
+            clickedTag = (int)clicked.Tag.GetType().GetProperty("tag").GetValue(clicked.Tag);
+            compareImages(showImage1, showImage2);
+
+        }
+
+        #endregion
+
+        #region TimerEvents
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            time--;
+            lblTime.Text = time.ToString();
+            if (time == 0)
+                gameOver("Uzgunum, kaybettiniz");
+            
+        }
+        private void timer2_Tick_1(object sender, EventArgs e)
+        {
+            string currentDirectory = getDirectoryPath();
+            if(clicked != null && clicked2 != null)
+            {
+                clicked.Image = Image.FromFile($"{currentDirectory}images/background.jpg");
+                clicked2.Image = Image.FromFile($"{currentDirectory}images/background.jpg");
+            }
+            
+            showImage1 = false;
+            showImage2 = false;
+            flwPictures.Enabled = true;
+            flwPictures2.Enabled = true;
+            timer2.Stop();
+
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            hideAllImages();
+            timer3.Stop();
+        }
+
+
+        #endregion
+
+        #endregion
+
+
+    }
+}
